c9b9a7665c19cead510e90685bee0452995a5f6a
Dmytro Lemeshchenko <dimmonishe@gmail.com>
2023-02-25 23:13:41 +0200

    Separate terminal connection

--- src/main/java/org/productenginetest/Main.java
+++ src/main/java/org/productenginetest/Main.java
@@ -1,14 +1,7 @@
 package org.productenginetest;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.Scanner;
 import lombok.extern.log4j.Log4j2;
 
@@ -43,21 +36,10 @@ public class Main {
         }
         scanner.close();
 
-        try(ServerSocket portSocket = new ServerSocket(terminalPort)) {
-            Socket socket = portSocket.accept();
-            InputStream inputStream = socket.getInputStream();
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
-            BufferedWriter bufferedWriter = new BufferedWriter(printWriter);
-            searchDepth = readSearchDepth(bufferedWriter, bufferedReader);
-            writeToTerminal(bufferedWriter, "Please enter the search mask: ");
-            searchMask = bufferedReader.readLine();
-            socket.close();
-        } catch (IOException e) {
-            throw new IOException("Can't connect to port " + terminalPort, e);
-        } catch (Exception e) {
-            throw new RuntimeException("Can't connect to terminal", e);
-        }
+        TelnetConnector telnetConnector = new TelnetConnector();
+        telnetConnector.terminalConnector(terminalPort);
+        searchDepth = telnetConnector.getSearchDepth();
+        searchMask = telnetConnector.getSearchMask();
 
         System.out.println("Root Path " + rootPath);
         System.out.println("Terminal port  " + terminalPort);
@@ -72,35 +54,5 @@ public class Main {
     private static String tryAgain() {
         System.out.print("Please try again! " + System.lineSeparator());
         return scanner.nextLine();
-    }
-
-    public static Integer readSearchDepth(BufferedWriter bufferedWriter,
-                                  BufferedReader bufferedReader) throws Exception {
-        int searchDepth = 0;
-        while (true) {
-            readDepth:
-            {
-                writeToTerminal(bufferedWriter, "Please enter the positive integer depth (0...50)"
-                        + " for search: ");
-                try {
-                    searchDepth = Integer.parseInt(bufferedReader.readLine());
-                    if (searchDepth < 0 || searchDepth > 50) {
-                        break readDepth;
-                    }
-                    break;
-                } catch (NumberFormatException e) {
-                    writeToTerminal(bufferedWriter,
-                            "Wrong number format, you should input a number");
-                }
-            }
-        }
-        return searchDepth;
-    }
-
-    private static void writeToTerminal(BufferedWriter bufferedWriter,
-                                        String message) throws Exception {
-        bufferedWriter.write(message);
-        bufferedWriter.write(System.lineSeparator());
-        bufferedWriter.flush();
     }
 }
